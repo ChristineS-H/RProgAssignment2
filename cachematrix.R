2b95e86b5db03f34e0e9fd320dd50fb4bf7d081f
Christine <86830716+ChristineS-H@users.noreply.github.com>
2021-08-03 17:40:20 +0200

    Update cachematrix.R
    Assingment 2 functions "makeCacheMatrix" and "cacheSolve" added to the file content.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverseM <- NULL
+        set <- function(y) {
+                x <<- y
+                inverseM <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) inverseM <<- solve(x) 
+        getinverse <- function() inverseM
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverseM <- x$getinverse()
+        if(!is.null(inverseM)) {
+                message("getting cached inverse matrix")
+                return(inverseM)
+        }
+        data <- x$get()
+        inverseM <- solve(data, ...)
+        x$setinverse(inverseM)
+        inverseM
 }
